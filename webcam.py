3e213e4232d075e557e0ee37e9706e6d8d429f95
Shell M. Shrader <shell@shellware.com>
2022-11-26 21:10:39 -0500

    formatting

--- webcam.py
+++ webcam.py
@@ -90,7 +90,6 @@ class WebRequestHandler(BaseHTTPRequestHandler):
             self.server.unlockEncoder()
             return
 
-
         self.send_response(404)
         self.send_header("Content-type", "text/html")
         self.end_headers()
